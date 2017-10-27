77e8cd53e991d7c7ab0f8328de236850c19f4133
Dio Brando <7iscool77@gmail.com>
2017-10-28 01:17:05 +0200

    R&D modulo
    check console after pressing the button

---
+++ 64bitensemble.js
@@ -0,0 +1,17 @@
+
+function reeksModulo(){
+  // function
+  for (i = 1; i < 101; i++) {
+    // for loop
+    if ((i % 3) == 0) {
+    console.log("Media");
+  } else if ((i % 5) == 0) {
+    console.log("College");
+  } else {
+    console.log(i);
+  };
+
+    // for loop
+  };
+  //function
+}
